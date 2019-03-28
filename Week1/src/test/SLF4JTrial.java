c823146fb1c3fc7ae5b36b1a100024f3490b6349
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-03-28 18:55:42 -0400

    tried log4j

--- Week1/src/test/SLF4JTrial.java
+++ Week1/src/test/SLF4JTrial.java
@@ -2,13 +2,21 @@ package test;
 //import java.util.HashMap; 
 //import java.util.Map;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.Level;
+
 public class SLF4JTrial {
 	private Logger logger;
+	private int countApples;
+	private String name;
+	private final static Level defaultLevel = Level.DEBUG;
 
-	public SLF4JTrial() {
-		logger = LoggerFactory.getLogger(SLF4JTrial.class);
+	public SLF4JTrial(int countApples, String name) {
+		this.countApples = countApples;
+		this.name = name;
+		logger = (Logger) LoggerFactory.getLogger(SLF4JTrial.class);
+		logger.setLevel(defaultLevel);
 	}
 	
 	public void warningTest() {
@@ -23,13 +31,24 @@ public class SLF4JTrial {
 		logger.error("ERROR!");
 	}
 	
+	public void debugTest() {
+		logger.debug("Debugging");
+	}
+	
+	public void specificError() {
+		logger.debug("{} has {} apples", name, countApples);
+	}
+	
+	
 	public static void main(String[] args) {
 //		Map<Character, Integer> map = new HashMap<>();
 //		Object a = new Object();
 //		a.
-		SLF4JTrial trial = new SLF4JTrial();
+		SLF4JTrial trial = new SLF4JTrial(30, "Tom");
+		trial.debugTest();
 		trial.infoTest();
 		trial.warningTest();
 		trial.errorTest();
+		trial.specificError();
 	}
 }
